7f7d9d9ca8a85598e2442b2247b5f4ffde91193f
halimatou-saddiyaa <halimatou.saddiyaa@gmail.com>
2025-10-18 19:12:15 +0100

    Add a user dropdown

--- script.js
+++ script.js
@@ -4,9 +4,53 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { getUserIds } from "./storage.js";
+import { getUserIds, getData, setData } from "./storage.js";
 
-window.onload = function () {
+/*window.onload = function () {
   const users = getUserIds();
   document.querySelector("body").innerText = `There are ${users.length} users`;
-};
+}; */
+
+const userSelect = document.getElementById("userSelect");
+const container = document.getElementById("bookmarksContainer");
+
+// --- Populate dropdown ---
+const userIds = getUserIds();
+userIds.forEach((id) => {
+  const option = document.createElement("option");
+  option.value = id;
+  option.textContent = `User ${id}`;
+  userSelect.appendChild(option);
+});
+
+// --- When user changes selection ---
+userSelect.addEventListener("change", () => {
+  const userId = userSelect.value;
+
+  // If nothing is selected, clear the container
+  if (!userId) {
+    container.innerHTML = "<p>Please select a user.</p>";
+    return;
+  }
+
+  const data = getData(userId);
+
+  // If no data stored, show message
+  if (!data || !data.bookmarks || data.bookmarks.length === 0) {
+    container.innerHTML = "<p>This user has no bookmarks yet.</p>";
+    return;
+  }
+
+  // Display the bookmarks
+  container.innerHTML = sorted
+    .map(
+      (b) => `
+        <div class="bookmark">
+          <h3><a href="${b.url}" target="_blank">${b.title}</a></h3>
+          <p>${b.description}</p>
+          <small>Added on ${b.created}</small>
+        </div>
+      `
+    )
+    .join("");
+});
